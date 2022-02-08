337e53155799565ec80c293352ba216b33832175
Kushagra Sharma <giantkillerkushagra@gmail.com>
2022-02-08 22:40:13 +0530

    first commit

---
+++ public/js/chat.js
@@ -0,0 +1,69 @@
+const socket = io();
+
+const $submitMessage = document.querySelector("#submitMessage");
+const $messages = document.querySelector("#messages");
+
+//-------------TEMPLATES
+
+const messageTemplate = document.querySelector("#message-template").innerHTML;
+const locationMessageTemplate = document.querySelector(
+  "#location-message-template"
+).innerHTML;
+
+socket.on("message", (message) => {
+  const html = Mustache.render(messageTemplate, {
+    message: message.text,
+    createdAt: moment(message.createdAt).format("h:mm a"),
+  });
+
+  $messages.insertAdjacentHTML("beforeend", html);
+});
+
+socket.on("locationMessage", (locationMessage) => {
+
+  const html = Mustache.render(locationMessageTemplate, {
+
+    locationMessage,
+    
+  });
+
+  $messages.insertAdjacentHTML("beforeend", html);
+});
+
+// send a message to the server from input
+const form = document.querySelector("#chatForm");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  $submitMessage.setAttribute("disabled", "disabled");
+
+  const message = document.querySelector("#chatInput").value;
+
+  socket.emit("sendMessage", message);
+
+  $submitMessage.removeAttribute("disabled");
+
+  document.querySelector("#chatInput").value = "";
+  document.querySelector("#chatInput").focus();
+});
+
+const loc = document.querySelector("#sendLocation");
+
+loc.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (!navigator.geolocation) {
+    return alert("Geolocation is not supported by your browser");
+  }
+
+  loc.querySelector("button").textContent = "Sending Location...";
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    loc.querySelector("button").textContent = "Send Location";
+    socket.emit("sendLocation", {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+    });
+  });
+});
---
+++ src/index.js
@@ -0,0 +1,53 @@
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+const {generateMessage} = require("./utils/messages");
+
+// --------------APP SETUP START-------------//
+
+const app = express();
+
+const server = http.createServer(app);
+
+const io = socketio(server);
+
+const port = process.env.PORT || 3000;
+
+const publicDirectoryPath = path.join(__dirname, "../public");
+
+app.use(express.static(publicDirectoryPath));
+
+//----------------APP SETUP END----------------//
+
+// ----------SOCKET SETUP START---------------//
+
+
+io.on("connection", (socket) => {
+  console.log("New WebSocket connection");
+
+  socket.emit("message", generateMessage("Welcome to the chat app!"));
+
+  socket.broadcast.emit("message", generateMessage("New user joined!"));
+
+  socket.on("sendMessage", (message) => {
+    io.emit("message", generateMessage(message));
+  });
+
+  socket.on("sendLocation", (coords) => {
+    io.emit("locationMessage", `https://google.com/maps?q=${coords.latitude},${coords.longitude}`);
+  });
+
+  socket.on("disconnect", () => {
+    io.emit("message", generateMessage("User has left the chat!"));
+    console.log("User disconnected");
+  })
+
+});
+
+// SOCKET ENDS
+
+// SERVER
+server.listen(port, () => {
+  console.log(`Server is up on port ${port}!`);
+});
